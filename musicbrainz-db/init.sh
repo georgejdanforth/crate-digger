f97eac54fe76ecb2d5b7ad18ffca0273c0f706d7
George Danforth <georgejdanforth@gmail.com>
2022-05-16 07:28:13 -0400

    Replace musicbrainz-server submodule with mbdata

--- musicbrainz-db/init.sh
+++ musicbrainz-db/init.sh
@@ -8,31 +8,38 @@ PG_DSN=${MUSICBRAINZ_DSN:-'postgresql://musicbrainz:musicbrainz@localhost:15432/
 echo "Creating schemata..."
 psql $PG_DSN --csv -c 'CREATE SCHEMA IF NOT EXISTS musicbrainz'
 
-echo "Instaling Extensions..."
-psql $PG_DSN --csv -f $SCRIPT_DIR/musicbrainz-server/admin/sql/Extensions.sql
+echo "Installing extensions..."
+psql $PG_DSN --csv -f $SCRIPT_DIR/mbdata/mbdata/sql/Extensions.sql > /dev/null
 echo "Creating collations..."
-psql $PG_DSN --csv -f $SCRIPT_DIR/musicbrainz-server/admin/sql/CreateCollations.sql
-echo "Creating types..."
-psql $PG_DSN --csv -f $SCRIPT_DIR/musicbrainz-server/admin/sql/CreateTypes.sql
+psql $PG_DSN --csv -f $SCRIPT_DIR/mbdata/mbdata/sql/CreateCollations.sql > /dev/null
 echo "Creating tables..."
-psql $PG_DSN --csv -f $SCRIPT_DIR/musicbrainz-server/admin/sql/CreateTables.sql
+psql $PG_DSN --csv -f $SCRIPT_DIR/mbdata/mbdata/sql/CreateTables.sql > /dev/null
+
+if [[ "$1" ]]
+then
+    echo "Loading data..."
+    $SCRIPT_DIR/load.sh $1
+else
+    echo "No data path given. Skipping load."
+fi
+
 echo "Creating primary keys..."
-psql $PG_DSN --csv -f $SCRIPT_DIR/musicbrainz-server/admin/sql/CreatePrimaryKeys.sql
+psql $PG_DSN --csv -f $SCRIPT_DIR/mbdata/mbdata/sql/CreatePrimaryKeys.sql > /dev/null
 echo "Creating search configuration..."
-psql $PG_DSN --csv -f $SCRIPT_DIR/musicbrainz-server/admin/sql/CreateSearchConfiguration.sql
+psql $PG_DSN --csv -f $SCRIPT_DIR/mbdata/mbdata/sql/CreateSearchConfiguration.sql > /dev/null
 echo "Creating functions..."
-psql $PG_DSN --csv -f $SCRIPT_DIR/musicbrainz-server/admin/sql/CreateFunctions.sql
+psql $PG_DSN --csv -f $SCRIPT_DIR/mbdata/mbdata/sql/CreateFunctions.sql > /dev/null
 echo "Creating indexes..."
-psql $PG_DSN --csv -f $SCRIPT_DIR/musicbrainz-server/admin/sql/CreateIndexes.sql
+psql $PG_DSN --csv -f $SCRIPT_DIR/mbdata/mbdata/sql/CreateIndexes.sql > /dev/null
+echo "Creating slave indexes..."
+psql $PG_DSN --csv -f $SCRIPT_DIR/mbdata/mbdata/sql/CreateSlaveIndexes.sql > /dev/null
 echo "Creating foreign key constraints..."
-psql $PG_DSN --csv -f $SCRIPT_DIR/musicbrainz-server/admin/sql/CreateFKConstraints.sql
+psql $PG_DSN --csv -f $SCRIPT_DIR/mbdata/mbdata/sql/CreateFKConstraints.sql > /dev/null
 echo "Creating foreign table constraints..."
-psql $PG_DSN --csv -f $SCRIPT_DIR/musicbrainz-server/admin/sql/CreateConstraints.sql
-echo "Setting raw initial sequence values..."
-psql $PG_DSN --csv -f $SCRIPT_DIR/musicbrainz-server/admin/sql/SetSequences.sql
+psql $PG_DSN --csv -f $SCRIPT_DIR/mbdata/mbdata/sql/CreateConstraints.sql > /dev/null
 echo "Creating views..."
-psql $PG_DSN --csv -f $SCRIPT_DIR/musicbrainz-server/admin/sql/CreateViews.sql
+psql $PG_DSN --csv -f $SCRIPT_DIR/mbdata/mbdata/sql/CreateViews.sql > /dev/null
 echo "Creating triggers..."
-psql $PG_DSN --csv -f $SCRIPT_DIR/musicbrainz-server/admin/sql/CreateTriggers.sql
+psql $PG_DSN --csv -f $SCRIPT_DIR/mbdata/mbdata/sql/CreateTriggers.sql > /dev/null
 echo "Creating search indexes..."
-psql $PG_DSN --csv -f $SCRIPT_DIR/musicbrainz-server/admin/sql/CreateSearchIndexes.sql
+psql $PG_DSN --csv -f $SCRIPT_DIR/mbdata/mbdata/sql/CreateSearchIndexes.sql > /dev/null
