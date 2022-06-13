a1ce6d972eb6d6ca86d860a3d4582f6d413baa31
George Danforth <georgejdanforth@gmail.com>
2022-06-13 06:48:21 -0400

    Add search result types to frontend

---
+++ frontend/models.ts
@@ -0,0 +1,44 @@
+export enum SearchType {
+  Artist = 'Artist',
+  Label = 'Label',
+}
+
+export interface SearchResults<T = ArtistSearchResult | LabelSearchResult> {
+  results: T[];
+}
+
+export interface ArtistSearchResult {
+  id: string;
+  gid: string;
+  name: string;
+  sortName: string
+  beginDateYear: number | null;
+  beginDateMonth: number | null;
+  beginDateDay: number | null;
+  endDateYear: number | null;
+  endDateMonth: number | null;
+  endDateDay: number | null;
+  type: number | null;
+  comment: string;
+  score: number;
+}
+
+export interface LabelSearchResult {
+  id: string;
+  gid: string;
+  name: string;
+  beginDateYear: number | null;
+  beginDateMonth: number | null;
+  beginDateDay: number | null;
+  endDateYear: number | null;
+  endDateMonth: number | null;
+  endDateDay: number | null;
+  labelCode: number | null;
+  type: number | null;
+  comment: string;
+  areaName: string;
+  score: number;
+}
+
+export type SearchResultType<T extends SearchType> =
+  T extends SearchType.Artist ? ArtistSearchResult : LabelSearchResult;
